9591c9eea0e64dfe2eab3a03aa4c96f69fb3ff7e
Andrew Kurin <andrew_kurin@mail.ru>
2025-10-18 01:23:10 +0300

    create simple model and admin panel for blog

---
+++ mysite/blog/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    tittle = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+
+    class Meta:
+        ordering = ['-publish'] #обратная сортировка от более свежего поста к раннему
+        indexes = [
+            models.Index(fields=['-publish']), #создание индекса БД в убывающем порядке
+        ]
+
+    def __str__(self):
+        return self.tittle
